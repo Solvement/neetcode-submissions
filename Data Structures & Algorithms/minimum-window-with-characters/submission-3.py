2fe4dd945cad3f009b11c4837fa452dcec6ffd40
Solvement <160995374+Solvement@users.noreply.github.com>
2026-09-09 20:39:36 -0400

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ""
+
+        target = Counter(t)
+        window = Counter()
+
+        l = 0
+        have = 0
+        need = len(target)
+
+        ansleft = -1
+        ansright = len(s)
+        
+
+        for r, ch in enumerate(s):
+            window[ch] += 1
+
+            if ch in target and window[ch] == target[ch]:
+                have += 1
+
+            while have == need:
+                if r - l < ansright-ansleft:
+                    ansleft = l
+                    ansright = r
+                    anslen = r - l + 1
+
+                window[s[l]] -= 1
+
+                if s[l] in target and window[s[l]] < target[s[l]]:
+                    have -= 1
+
+                l += 1
+
+        if ansleft == -1:
+            return ""
+
+        return s[ansleft:ansright + 1]
